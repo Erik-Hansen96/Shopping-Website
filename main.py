13706a3fd206cc08916684d022ebfdb481e8c41e
Erik <ehansen1671@gmail.com>
2023-11-11 15:03:20 -0800

    first commit with flask

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask,redirect,url_for,render_template
+### WSGI Application
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "woooooooo"
+
+@app.route('/success/<int:score>')
+def success(score):
+    return render_template('index.html')
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "Sorry. the score is "+ str(score)
+
+### result checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result=""
+    if marks<50:
+        result='fail'
+    else:
+        result='success'
+    return redirect(url_for(result,score=marks))
+
+
+if __name__=='__main__':
+    app.run(debug=True)
