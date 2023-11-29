d10d12ba8f369c318165c7e1d91f4df7b6eb8c2d
Erik <ehansen1671@gmail.com>
2023-11-28 23:21:01 -0800

    final? push

--- flaskr/shop.py
+++ flaskr/shop.py
@@ -10,13 +10,7 @@ bp = Blueprint('shop', __name__)
 
 @bp.route('/')
 def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('base.html', posts=posts)
+    return render_template('base.html')
 
 @bp.route('/product_detail/<int:product_id>')
 def product_detail(product_id):
